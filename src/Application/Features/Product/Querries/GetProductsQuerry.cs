1b04b8fd3c3be5dca23fb284656a76d3e663c5c2
dimauloagustin <dimauloagustin@gmail.com>
2021-01-20 16:39:46 -0300

    paginated querryfor product added

---
+++ src/Application/Features/Product/Querries/GetProductsQuerry.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.Product.Responses;
+using Application.Interfaces.Repositories;
+using AutoMapper;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Product.Querries
+{
+    public class GetProductsQuerry : IRequest<List<ProductResponse>>
+    {
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+    }
+
+    public class GetProductCommandHandler : IRequestHandler<GetProductsQuerry, List<ProductResponse>>
+    {
+        private readonly IProductRepository _ProductRepository;
+        private readonly IMapper _mapper;
+        public GetProductCommandHandler(IProductRepository ProductRepository, IMapper mapper)
+        {
+            _ProductRepository = ProductRepository;
+            _mapper = mapper;
+        }
+
+        public Task<List<ProductResponse>> Handle(GetProductsQuerry request, CancellationToken cancellationToken)
+        {
+            var response = _ProductRepository.All().Skip(request.PageIndex * request.PageSize).Take(request.PageSize).Select(r => _mapper.Map<ProductResponse>(r)).ToList();
+            return Task.FromResult(response);
+        }
+    }
+}
