2f49b54d585673c3c4a32f107c57fafdf1a5f911
dimauloagustin <dimauloagustin@gmail.com>
2021-01-22 15:49:23 -0300

    Add session entity

---
+++ src/Application/Entities/Session.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Application.Entities
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public int UserId { get; set; }
+        public virtual Domain.Entities.User User { get; set; }
+    }
+}
