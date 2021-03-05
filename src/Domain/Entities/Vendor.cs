fd9affcc51a371f60709f1306eb07d791d15f456
dimauloagustin <dimauloagustin@gmail.com>
2021-03-05 15:28:10 -0300

    Add vendor entity and Response

---
+++ src/Domain/Entities/Vendor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Vendor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AboutUsDescription { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string CoordinateX { get; set; }
+        public string CoordinateY { get; set; }
+        public bool Active { get; set; } = true;
+    }
+}
