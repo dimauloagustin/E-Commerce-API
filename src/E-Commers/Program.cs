f074e38b09c243b477eed7ce881bd6ba96bc2e40
dimauloagustin <dimauloagustin@gmail.com>
2021-03-06 21:02:21 -0300

    remove unused dependencies

--- src/E-Commers/Program.cs
+++ src/E-Commers/Program.cs
@@ -1,14 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Serilog.Extensions.Logging;
 
 namespace E_Commers
 {
