6722aaae2a4f6837537e0865ef1c22bc2db8427b
dimauloagustin <dimauloagustin@gmail.com>
2021-04-23 11:53:50 -0300

    remove old implementation

--- src/Application/Exceptions/Abstractions/IJsonResultConvertible.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Application.Exceptions.Abstractions
-{
-    public interface IJsonResultConvertible
-    {
-        public ObjectResult ToJsonResult(string requestId);
-    }
-}
