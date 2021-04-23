53cfd91a5fdd15ac6088f7d04164b4445204fc30
dimauloagustin <dimauloagustin@gmail.com>
2021-04-23 11:57:54 -0300

    configure error manager

---
+++ src/E-Commers/Errors/ErrorManagerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Application.Exceptions;
+using E_Commers.Errors.ErrorResponses;
+
+namespace E_Commers.Errors
+{
+    public class ErrorManagerConfiguration
+    {
+        public ErrorManager Configure(ErrorManager em)
+        {
+            return em.MapErrorToException<ModelValidationException, ModelValidationErrorResponse>();
+        }
+    }
+}
