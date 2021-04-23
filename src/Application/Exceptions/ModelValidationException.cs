ab20d396b36bfcf01f4f9f481f5716979d6e2224
dimauloagustin <dimauloagustin@gmail.com>
2021-04-23 11:56:18 -0300

    Simplify custom exception format

--- src/Application/Exceptions/ModelValidationException.cs
+++ src/Application/Exceptions/ModelValidationException.cs
@@ -1,20 +1,15 @@
-﻿using Application.Exceptions.Abstractions;
-using Application.Exceptions.Responses;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Application.Exceptions
 {
-    public class ModelValidationException : Exception, IJsonResultConvertible
+    public class ModelValidationException : Exception
     {
         public List<ValidationResult> ValidationErrors { get; set; }
         public ModelValidationException(List<ValidationResult> validationResults) : base("Validation error")
         {
             ValidationErrors = validationResults;
         }
-
-        public ObjectResult ToJsonResult(string requestId) => new UnprocessableEntityObjectResult(new ModelValidationErrorResponse(ValidationErrors) { RequestId = requestId });
     }
 }
