e5e101c602b1148f3500283430872d6cf500a173
dimauloagustin <dimauloagustin@gmail.com>
2021-04-23 21:39:19 -0300

    re implement exception

--- src/Application/Exceptions/ModelValidationException.cs
+++ src/Application/Exceptions/ModelValidationException.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace Application.Exceptions
 {
+    [Serializable]
     public class ModelValidationException : Exception
     {
         public List<ValidationResult> ValidationErrors { get; set; }
         public ModelValidationException(List<ValidationResult> validationResults) : base("Validation error")
         {
             ValidationErrors = validationResults;
+        }
+
+        public ModelValidationException(string message) : base(message) { }
+
+        public ModelValidationException(string message, Exception innerException) : base(message, innerException) { }
+
+        public ModelValidationException(string message, List<ValidationResult> validationResults) : base(message)
+        {
+            ValidationErrors = validationResults;
+        }
+
+        public ModelValidationException(string message, List<ValidationResult> validationResults, Exception innerException) : base(message, innerException)
+        {
+            ValidationErrors = validationResults;
+        }
+
+        protected ModelValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            this.ValidationErrors = (List<ValidationResult>)info.GetValue("ValidationErrors", typeof(List<ValidationResult>));
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException(nameof(info));
+            }
+
+            info.AddValue("ValidationErrors", this.ValidationErrors, typeof(List<ValidationResult>));
+
+            base.GetObjectData(info, context);
         }
     }
 }
