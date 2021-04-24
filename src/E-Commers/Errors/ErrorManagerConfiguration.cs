eefb519ecbc008e1d622ed603c780b3bf1fcff35
dimauloagustin <dimauloagustin@gmail.com>
2021-04-23 21:12:58 -0300

    exclude config from Test coverage

--- src/E-Commers/Errors/ErrorManagerConfiguration.cs
+++ src/E-Commers/Errors/ErrorManagerConfiguration.cs
@@ -1,8 +1,10 @@
 ﻿using Application.Exceptions;
 using E_Commers.Errors.ErrorResponses;
+using System.Diagnostics.CodeAnalysis;
 
 namespace E_Commers.Errors
 {
+    [ExcludeFromCodeCoverage]
     public class ErrorManagerConfiguration
     {
         public ErrorManager Configure(ErrorManager em)
