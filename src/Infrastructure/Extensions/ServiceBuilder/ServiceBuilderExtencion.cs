673f9aaef66038cb3b1663d0a95563c16016e92f
dimauloagustin <dimauloagustin@gmail.com>
2021-05-07 16:23:07 -0300

    added dependencies to injector

--- src/Infrastructure/Extensions/ServiceBuilder/ServiceBuilderExtencion.cs
+++ src/Infrastructure/Extensions/ServiceBuilder/ServiceBuilderExtencion.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Interfaces.Repositories;
 using Application.Interfaces.Services;
 using Infrastructure.Contexts;
+using Infrastructure.Dependencies;
+using Infrastructure.Dependencies.Abstractions;
 using Infrastructure.Repository.EfCore;
 using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +15,10 @@ namespace Infrastructure.Extensions.ServiceBuilder
     {
         public static void AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddTransient<IFileStreamFactory, RealFileStreamFactory>();
+            services.AddTransient<IFileSystemProvider, RealFileSystemProvider>();
+            services.AddTransient<ILinkProvider, LinkProvider>();
+
             services.AddDbContext<BasicDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("E-Commers")));
 
