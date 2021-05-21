21213e29e1f29e6266e69ad8968acd37a2cbf037
dimauloagustin <dimauloagustin@gmail.com>
2021-05-20 23:31:00 -0300

    implement service

---
+++ src/Infrastructure/Services/HashService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces.Services;
+
+namespace Infrastructure.Services
+{
+    public class HashService : IHashService
+    {
+        public string Hash(string value)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(value);
+        }
+
+        public bool Verify(string value, string hashedValue)
+        {
+            return BCrypt.Net.BCrypt.Verify(value, hashedValue);
+        }
+    }
+}
