dc46d0ddb2ce4a77ff2e8e1672ea9de183ad7b3e
dimauloagustin <dimauloagustin@gmail.com>
2021-05-21 22:21:21 -0300

    service test

---
+++ src/test/Test.Infrastructure/Services/HashServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Dependencies.Abstractions;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test.Infrastructure.Services
+{
+    public class HashServiceTest
+    {
+        [Fact]
+        public void Should_hash_and_verify()
+        {
+            // Arrange
+            string pass = "new pass";
+            HashService service = new HashService();
+
+            // Act
+            string hash = service.Hash(pass);
+            bool res = service.Verify(pass, hash);
+
+            // Assert
+            Assert.True(res);
+        }
+    }
+}
