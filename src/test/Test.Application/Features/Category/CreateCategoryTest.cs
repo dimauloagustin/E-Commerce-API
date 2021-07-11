a817c64de776190f86237011b46101e9db56f09e
Agustin, Di Maulo <dimauloagustin@gmail.com>
2021-07-11 12:10:19 -0300

    added create category test

---
+++ src/test/Test.Application/Features/Category/CreateCategoryTest.cs
@@ -0,0 +1,46 @@
+﻿using Application.Commands.Categories;
+using Application.Features.Product;
+using Application.Interfaces.Repositories;
+using Application.Mappings;
+using AutoMapper;
+using Domain.Entities;
+using FluentAssertions;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test.Application.Features.Products
+{
+    public class CreateCategoryTest
+    {
+        [Fact]
+        public void Should_create_category_without_parent()
+        {
+            // Arrange
+            var command = new CreateCategory
+            { 
+                Name = "test category"
+            };
+
+            var entity = new Category { Id = 1, Name = command.Name };
+
+            //TODO - move mapper to fixture
+            var mapperConfig = new MapperConfiguration(opts =>
+            {
+                opts.AddProfile<GeneralProfile>();
+            });
+
+            var mapper = mapperConfig.CreateMapper();
+
+            var fakeRepo = new Mock<ICategoryRepository>();
+            fakeRepo.Setup(m => m.AddAsync(It.IsAny<Category>())).Returns(Task.FromResult(entity));
+
+            // Act
+            var res = Task.Run(() => new CreateCategoryHandler(fakeRepo.Object, mapper).Handle(command, default)).Result;
+
+            // Assert
+            fakeRepo.Verify(x => x.AddAsync(It.IsAny<Category>()), Times.Once());
+            mapper.Map<Category>(entity).Should().BeEquivalentTo(res);
+        }
+    }
+}
